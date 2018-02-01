735ff0f50697843ba6fc9f89cb7b9b97e7ea0fc5
liuyonghui <1359158019@qq.com>
2018-02-02 00:49:19 +0800

    Seeding Post and fix a bug

--- app/Post.php
+++ app/Post.php
@@ -8,11 +8,11 @@ class Post extends Model
 {
     protected $dates = ['published_at'];
 
-    public function seTitleAttribute($value)
+    public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
 
-        if(! $this->exists){
+        if (! $this->exists) {
             $this->attributes['slug'] = str_slug($value);
         }
     }
