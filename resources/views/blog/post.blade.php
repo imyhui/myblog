11b06da4fc1ac067ca60873e553064d335ce409f
liuyonghui <1359158019@qq.com>
2018-02-02 01:27:14 +0800

    Finish index and post page

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: andyhui
- * Date: 18-2-2
- * Time: 上午1:12
- */
+<html>
+    <head>
+        <title>{{ $post->title }}</title>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <h1>{{ $post->title }}</h1>
+            <h5>{{ $post->published_at }}</h5>
+            <hr>
+            {!! nl2br(e($post->content)) !!}
+            <hr>
+            <button class="btn btn-primary" onclick="history.go(-1)">
+                « Back
+            </button>
+        </div>
+    </body>
+</html>
