3479853c315b0467f717b119ccd51a6d7efaf479
liuyonghui <1359158019@qq.com>
2018-02-02 23:40:21 +0800

    Finish Post part except vier

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = [
+        'tag', 'title', 'subtitle', 'page_image', 'meta_description','reverse_direction',
+    ];
+
+    /**
+     * 定义文章与标签之间多对多关联关系
+     *
+     * @return BelongsToMany
+     */
+    public static function addNeededTags(array $tags)
+    {
+        if (count($tags) === 0) {
+            return;
+        }
+
+        $found = static::whereIn('tag', $tags)->lists('tag')->all();
+
+        foreach (array_diff($tags, $found) as $tag) {
+            static::create([
+                'tag' => $tag,
+                'title' => $tag,
+                'subtitle' => 'Subtitle for '.$tag,
+                'page_image' => '',
+                'meta_description' => '',
+                'reverse_direction' => false,
+            ]);
+        }
+    }
+
+
 }
